ec8a5252c879ddd6c6331ee53a2174f0727728b3
Gabriel Pereira <adm1nkash1m1r@gmail.com>
2023-05-08 11:18:34 -0300

    frontcontroller2

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 
 if(!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/'){
     require_once 'listagem-videos.php';
-}elseif($_SERVER['PATH_INFO'] === '/novo-curso'){
+}elseif($_SERVER['PATH_INFO'] === '/novo-video'){
     if($_SERVER['REQUEST_METHOD'] === 'GET'){
-        require_once 'formulario.php';
+        require_once 'fomulario.php';
     }elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
         require_once 'novo-video.php';
     }
@@ -17,6 +17,6 @@ if(!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/'){
     }elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
         require_once 'editar-video.php';
     }
-}elseif($_SERVER['PATH_INFO'] === 'remover-video'){
+}elseif($_SERVER['PATH_INFO'] === '/remover-video'){
     require_once 'remover-video.php';
 }
